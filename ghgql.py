3b6a95accc25289e28979ecd678a0eb4329a309e
Roni Choudhury <roni.choudhury@kitware.com>
2021-12-01 19:45:57 -0500

    Count and print issues in target repo

--- ghgql.py
+++ ghgql.py
@@ -1,11 +1,76 @@
 import click
 from pprint import pprint
 import requests
+from requests import api
 
 
 def get_api_key(filename):
     with open(filename) as f:
         return f.read().strip()
+
+
+def get_project_info(api_key, organization, project):
+    result = run_query(api_key, f"""
+        query {{
+            organization(login: "{organization}") {{
+                projectNext(number: {project}) {{
+                    title
+                    id
+                }}
+            }}
+        }}
+    """)
+
+    return result["data"]["organization"]["projectNext"]
+
+
+def get_open_issues(api_key, organization, repo):
+    result = run_query(api_key, f"""
+        query {{
+            repository(owner: "{organization}", name: "{repo}") {{
+                issues(states: [OPEN], first: 10) {{
+                    edges {{
+                        cursor
+                        node {{
+                            title
+                            id
+                        }}
+                    }}
+                }}
+            }}
+        }}
+    """)
+
+    data = result["data"]["repository"]["issues"]["edges"]
+    titles = [v["node"]["title"] for v in data]
+    cursor = data[-1]["cursor"]
+
+    while True:
+        result = run_query(api_key, f"""
+            query {{
+                repository(owner: "{organization}", name: "{repo}") {{
+                    issues(states: [OPEN], first: 50, after: "{cursor}") {{
+                        edges {{
+                            cursor
+                            node {{
+                                title
+                                id
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+        """)
+
+        data = result["data"]["repository"]["issues"]["edges"]
+
+        if len(data) == 0:
+            break
+
+        titles += [v["node"]["title"] for v in data]
+        cursor = data[-1]["cursor"]
+    
+    return titles
 
 
 def run_query(api_key, query):
@@ -22,24 +87,17 @@ def run_query(api_key, query):
 
 @click.command()
 @click.option("-c", "--credential-file", type=click.Path(), required=True)
-def main(credential_file):
+@click.option("-o", "--organization", type=str, required=True)
+@click.option("-r", "--repo", type=str, required=True)
+@click.option("-p", "--project", type=int, required=True)
+def main(credential_file, organization, repo, project):
     api_key = get_api_key(credential_file)
 
-    test_query = """
-    {
-        viewer {
-            login
-        }
-        rateLimit {
-            limit
-            cost
-            remaining
-            resetAt
-        }
-    }
-    """
+    pprint(get_project_info(api_key, organization, project))
 
-    pprint(run_query(api_key, test_query))
+    open_issues = get_open_issues(api_key, organization, repo)
+    pprint(open_issues)
+    print(len(open_issues))
 
 
 if __name__ == "__main__":
